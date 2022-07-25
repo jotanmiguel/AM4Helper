2e16d770be4205432e3f7f62076ba9cbc7b3c86c
João Oliveira <skreibar2pt@gmail.com>
2022-07-25 15:08:09 +0100

    new cargo calculator

---
+++ CargoHelper.py
@@ -0,0 +1,35 @@
+
+import math
+
+while True:
+    dist = int(input("Distância: "))
+    print(str(dist) + " kms")
+
+    tempo = int(input("Tempo em mins: "))
+    viagens = math.ceil(1080 / (tempo * 0.9))
+    print(str(tempo) + "mins = " + str(viagens) + " viagens\dia")
+
+    large = int(input("Demand Large Load: "))
+    largeviag = math.floor(large / viagens)
+    print("large: " + str(large) + " cargo/dia | " + str(largeviag) + " cargo/viagem")
+
+    heavy = int(input("Demand Heavy Load: "))
+    heavyviag = math.floor(heavy / viagens)
+    print("heavy: " + str(heavy) + " cargo/dia | " + str(heavyviag) + " cargo/viagem")
+
+
+    maxcargo = largeviag + heavyviag
+
+    print("\n")
+
+    print(
+        "Configuração ideal: "
+        + str(maxcargo)
+        + "pax -> "
+        + str(largeviag)
+        + "large "
+        + str(heavyviag)
+        + "heavy"
+    )
+    print("-------")
+    
