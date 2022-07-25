22a078e5b12a3ba3cbda91af65d2c68e047e9de7
João Oliveira <skreibar2pt@gmail.com>
2022-07-25 15:08:17 +0100

    a

--- fleet.py
+++ fleet.py
@@ -16,8 +16,19 @@ def FleetSize():
         int: Size of the fleet.
     """
     fleet = open("./data/fleet.txt", "r")
-    fleet = fleet.readlines()[1:]
+    fleet = fleet.readlines[1:]
     return len(fleet)
+
+def HighestIndex():
+    """
+    Returns the highest index of the fleet.
+
+    Returns:
+        int: Index.
+    """
+    fleet = open("./data/fleet.txt", "r")
+    fleet = fleet.readlines()[1:]
+    return int(max([line[1] for line in fleet]))
 
 
 def AddtoFleet(aircraft: ac.Aircraft):
@@ -33,9 +44,10 @@ def AddtoFleet(aircraft: ac.Aircraft):
     #
     fleet = open("./data/fleet.txt", "a")
     fleet = fleet.write(
-        " " + str(FleetSize() + 1) + " -> " + str(aircraft) + "\n"
+        " " + str(HighestIndex() + 1) + " -> " + str(aircraft) + "\n"
     )  # change the aircraft representation to a more coherent form
     return "added"
+
 
 def ResetFleet():
     """
@@ -47,3 +59,28 @@ def ResetFleet():
     fleet = open("./data/fleet.txt", "w")
     fleet = fleet.write("Fleet:\n")
     return "done"
+
+
+def RemovefromFleet(index: int):
+    fleet = open("./data/fleet.txt", "r")
+    fleet = fleet.readlines()
+    fleet.pop(index)
+    for line in fleet[index:]:
+        line.replace(line, UpdateIndex(line, int(line[1]) - 1))
+    saveFleet = open("./data/fleet.txt", "w")
+    saveFleet.writelines(fleet)
+    return fleet
+
+
+def UpdateIndex(line: str, newIndex: int):
+    """
+    simple update fuction to replace the index from the fleet to a new given one.
+
+    Args:
+        line (str): Line from from the fleet file.
+        newIndex (int): New index to be set.
+
+    Returns:
+        str: Returns the new line.
+    """
+    return line.replace(line, line[0] + str(newIndex) + line[2:])
